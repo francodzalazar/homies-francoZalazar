7acbf23d7373a6be0d70872440172d90ae393575
fdz21 <francodzalazar@gmail.com>
2022-10-04 09:55:39 -0300

    funcion generar producto

--- js/main.js
+++ js/main.js
@@ -1,43 +1,105 @@
 // Funciones
-// function costoExtras(extras) {
-//   if (extras == "SI" || extras == "Si" || extras == "si") {
-//     for (let i = 3; i > 0; i--) {
-//       let extras = parseInt(
-//         prompt("Ingrese la cantidad de extras a agregar (Max 5.)")
-//       );
-//       if (extras <= 5 && extras >= 1) {
-//         for (let i = 0; i <= extras; i++) {
-//           valorExtras = i * 50;
-//         }
-//         i = 0;
-//       } else {
-//         alert("Opcion invalida, te quedan " + (i - 1) + " intentos");
-//       }
-//     }
-//   } else {
-//     valorExtras = 0;
-//   }
-//   return valorExtras;
-// }
-// function costoEnvio(envio) {
-//   if (envio == 2) {
-//     valorEnvio = 100;
-//   } else {
-//     valorEnvio = 0;
-//   }
-//   return valorEnvio;
-// }
-// let precioTotal = (producto, extras, envio) => producto + extras + envio;
-// // Fin funciones
+function costoExtras(extras) {
+  if (extras.toLowerCase() == "si") {
+    for (let i = 3; i > 0; i--) {
+      let extras = parseInt(
+        prompt("Ingrese la cantidad de extras a agregar (Max 5.)")
+      );
+      if (extras <= 5 && extras >= 1) {
+        for (let i = 0; i <= extras; i++) {
+          valorExtras = i * 50;
+        }
+        i = 0;
+      } else {
+        alert("Opcion invalida, te quedan " + (i - 1) + " intentos");
+      }
+    }
+  } else {
+    valorExtras = 0;
+  }
+  return valorExtras;
+}
+function costoEnvio(envio) {
+  if (envio == 2) {
+    valorEnvio = 100;
+  } else {
+    valorEnvio = 0;
+  }
+  return valorEnvio;
+}
+let precioTotal = (producto, extras, envio) => producto + extras + envio;
+function generarProducto(producto) {
+  switch (producto) {
+    case "HAMBURGUESA":
+      let hamburguesa =
+        producto +
+        " " +
+        prompt("Elige una opcion \n- Simple \n- Doble \n- Triple");
+      return hamburguesa;
+    case "LOMITO":
+      let lomito =
+        producto + " " + prompt("Elige una opcion \n- Simple \n- Completo");
+      return lomito;
+    case "PAPAS":
+      let papas =
+        producto + " " + prompt("Elige una opcion \n- Cheddar \n- Huevo");
+      return papas;
+    case "BEBIDA":
+      let bebida =
+        producto +
+        " " +
+        prompt("Elige una opcion \n- Agua \n- Gaseosa \n- Cerveza");
+      return bebida;
 
-// // Variables
-// let producto = parseInt(
-//   prompt("¿Que vas a consumir? \n1 Hamburguesa \n2 Lomito \n3 Papas con beacon")
-// );
-// let precioHamburguesa = 1000;
-// let precioLomito = 1200;
-// let precioPapas = 850;
-// // Fin variables
+    default:
+      alert("Opción no disponible");
+      break;
+  }
+}
+function clasificar(producto, tipo, array) {
+  if (tipo.toLowerCase() == "comida") {
+    array.push(producto);
+  } else if (tipo.toLowerCase() == "bebida") {
+    array.push(producto);
+  }
+}
+// Fin funciones
+
+// Clases
+class Producto {
+  constructor(nombre, clasificacion, precio) {
+    this.nombre = nombre.toUpperCase();
+    this.clasificacion = clasificacion;
+    this.precio = parseFloat(precio);
+  }
+}
+// Fin clases
+
+// Variables
+let contador = 0;
+let validarEntrada = true;
+// Fin variables
+
+// Ciclos
+while (validarEntrada) {
+  let productoCliente = prompt(
+    "¿Que vas a consumir? \n- Hamburguesa \n- Lomito \n- Papas \n- Bebida"
+  );
+
+  const producto = new Producto(
+    generarProducto(productoCliente.toUpperCase()),
+    productoCliente,
+    1000
+  );
+  console.log(producto);
+  let nuevoProducto = prompt("Deseas agregar mas productos? SI - NO");
+  if (nuevoProducto.toUpperCase() == "SI") {
+    contador++;
+  } else {
+    validarEntrada = false;
+  }
+}
+// Fin ciclos
 
 // Condicionales
 // if (producto > 0 && producto < 4) {
@@ -48,7 +110,8 @@
 //       "Elige tu metodo de retiro: \n1 Retiro en el local \n2 Necesito que me lo envien"
 //     )
 //   );
-//   let precioEnvio = costoEnvio(envio);
+// }
+// let precioEnvio = costoEnvio(envio);
 //   switch (producto) {
 //     case 1:
 //       alert("Elegiste Hamburguesa");
@@ -89,58 +152,49 @@
 //   alert("Opcion no disponible");
 // }
 // Fin condicionales
-function clasificar(producto, tipo, array) {
-  if (tipo.toLowerCase() == "comida") {
-    array.push(producto);
-  } else if (tipo.toLowerCase() == "bebida") {
-    array.push(producto);
-  }
-}
+
 // Arrays
-const comidas = [];
-const bebidas = [];
+// const comidas = [];
+// const bebidas = [];
 // Fin Arrays
 
-// Clases
-class Producto {
-  constructor(nombre, clasificacion, tipo, precio) {
-    this.nombre = nombre.toUpperCase();
-    this.clasificacion = clasificacion;
-    this.tipo = tipo;
-    this.precio = parseFloat(precio);
-  }
-}
-// Fin clases
-
 // Objetos
-const producto1 = new Producto(
-  "hamburguesa simple",
-  "hamburguesa",
-  "comida",
-  850
-);
-const producto2 = new Producto(
-  "lomito simple",
-  "lomito",
-  "comida",
-  1050
-);
-const producto3 = new Producto(
-  "coca 500 ml",
-  "bebida sin alcohol",
-  "bebida",
-  500
-);
+// const producto1 = new Producto(
+//   "hamburguesa simple",
+//   "hamburguesa",
+//   "comida",
+//   850
+// );
+// const producto2 = new Producto(
+//   "hamburguesa doble",
+//   "hamburguesa",
+//   "comida",
+//   950
+// );
+// const producto3 = new Producto("lomito simple", "lomito", "comida", 1050);
+// const producto4 = new Producto("lomito completo", "lomito", "comida", 1150);
+// const producto5 = new Producto(
+//   "coca 500 ml",
+//   "bebida sin alcohol",
+//   "bebida",
+//   500
+// );
 
-clasificar(producto1, producto1.tipo, comidas);
-clasificar(producto2, producto2.tipo, comidas);
-clasificar(producto3, producto3.tipo, bebidas);
-console.log(comidas);
-console.log(bebidas);
+// clasificar(producto1, producto1.tipo, comidas);
+// clasificar(producto2, producto2.tipo, comidas);
+// clasificar(producto3, producto3.tipo, comidas);
+// clasificar(producto4, producto4.tipo, comidas);
+// clasificar(producto5, producto5.tipo, bebidas);
+// console.log(comidas);
+// console.log(bebidas);
 
-for (const comida of comidas) {
-  console.log("Se agregó el producto "+comida.nombre+", su precio es "+comida.precio);
-}
-for (const bebida of bebidas) {
-  console.log("Se agregó el producto "+bebida.nombre+", su precio es "+bebida.precio);
-}
+// for (const comida of comidas) {
+//   console.log(
+//     "Se agregó el producto " + comida.nombre + ", su precio es " + comida.precio
+//   );
+// }
+// for (const bebida of bebidas) {
+//   console.log(
+//     "Se agregó el producto " + bebida.nombre + ", su precio es " + bebida.precio
+//   );
+// }
